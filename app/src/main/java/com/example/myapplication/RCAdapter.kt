4b7da396794b36e25717ebd0e11ed2231318367f
stado-y <psybb228@gmail.com>
2021-11-14 19:12:00 +0200

    add custom RCview adapter

---
+++ app/src/main/java/com/example/myapplication/RCAdapter.kt
@@ -0,0 +1,63 @@
+package com.example.myapplication
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+
+class RCAdapter(listArray:ArrayList<ListItem>, context: Context):
+    RecyclerView.Adapter<RCAdapter.ViewHolder>() {
+
+    var RClistArray = listArray
+    var RCcontext = context
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val listImage = view.findViewById<ImageView>(R.id.itemImage)
+        val listTitle = view.findViewById<TextView>(R.id.itemTitle)
+        val listText = view.findViewById<TextView>(R.id.itemText)
+
+
+        fun bind(listitem: ListItem, context: Context) {
+
+            listImage.setImageResource(listitem.item_image_id)
+            listTitle.text = listitem.item_title
+            listText.text = listitem.item_text
+
+            itemView.setOnClickListener {
+
+                Toast.makeText(context, "${ listTitle.text }", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val inflater = LayoutInflater.from(RCcontext)
+        return ViewHolder(inflater.inflate(R.layout.item_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val listItem = RClistArray[position]
+        holder.bind(listItem, RCcontext)
+    }
+
+    override fun getItemCount(): Int {
+
+        return RClistArray.size
+    }
+
+    fun updateAdapter(listArray: ArrayList<ListItem>) {
+
+        RClistArray.clear()
+        RClistArray.addAll(listArray)
+        notifyDataSetChanged()
+    }
+}
+
